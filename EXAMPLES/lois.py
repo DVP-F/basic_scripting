374822af5677d574f54b85097308463edbf1d3e4
DVP-F <carnx@duck.com>
2025-03-20 22:16:22 +1300

    make lil bit safer and more accurate :3c

--- EXAMPLES/lois.py
+++ EXAMPLES/lois.py
@@ -2,7 +2,7 @@ from scapy.all import IP, TCP, send # pip install scapy --python C:\path\to\pyth
 
 # Define the target IP address and list of port numbers
 target_ip = "192.168.1.2"
-target_ports = [8080, 37000, 3659, 1024, 1124, 3216, 9960, 9969, 18000, 18120, 18060, 27900, 28910, 29900, 40000, 64000]
+target_ports = [22, 80, 420, 443, 5560, 6969]
 
 # Create a TCP SYN packet
 packet = IP(dst=target_ip) / TCP(dport=target_ports, flags="S")
@@ -10,7 +10,7 @@ packet = IP(dst=target_ip) / TCP(dport=target_ports, flags="S")
 # Send packets and display feedback
 sent_count = 0
 for port in target_ports:
-    for i in range(10000):
+    for i in range(10):
         send(packet)
         sent_count += 1
         print(f"Sent packet {sent_count} to port {port}")
